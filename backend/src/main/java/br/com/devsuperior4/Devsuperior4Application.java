32bcbc34da3140e5ca9b512c000e96f681d8534a
Airton Oliveira <airtonoliveira@users.noreply.github.com>
2021-09-09 01:02:00 -0300

    Project created

---
+++ backend/src/main/java/br/com/devsuperior4/Devsuperior4Application.java
@@ -0,0 +1,13 @@
+package br.com.devsuperior4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devsuperior4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devsuperior4Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/devsuperior4/Devsuperior4ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devsuperior4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devsuperior4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
